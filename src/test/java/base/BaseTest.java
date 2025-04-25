38ef6894659c930088a9eb1b1e96dd9b17e95119
Stylianos Gkiokas <sgkiokas@trading-point.com>
2025-04-25 10:45:57 +0300

    fix: version catalog addition and lib version bump

--- src/test/java/base/BaseTest.java
+++ src/test/java/base/BaseTest.java
@@ -7,16 +7,16 @@ import org.testng.annotations.BeforeTest;
 import ui.DriverManager;
 
 public abstract class BaseTest {
-    protected WebDriver driver;
+  protected WebDriver driver;
 
-    @BeforeTest(alwaysRun = true)
-    public void setUp() {
-        driver = DriverManager.getDriver();
-        driver.get(ConfigurationManager.getBaseUrl());
-    }
+  @BeforeTest(alwaysRun = true)
+  public void setUp() {
+    driver = DriverManager.getDriver();
+    driver.get(ConfigurationManager.getBaseUrl());
+  }
 
-    @AfterTest
-    public void tearDown() {
-        DriverManager.closeDriver();
-    }
+  @AfterTest
+  public void tearDown() {
+    DriverManager.closeDriver();
+  }
 }
--- src/test/java/ui/tests/SimpleUITest.java
+++ src/test/java/ui/tests/SimpleUITest.java
@@ -4,8 +4,8 @@ import base.BaseTest;
 import org.testng.annotations.Test;
 
 public class SimpleUITest extends BaseTest {
-    @Test
-    public void simpleUiScenario() {
-        System.out.println("Simple UI scenario example");
-    }
+  @Test
+  public void simpleUiScenario() {
+    System.out.println("Simple UI scenario example");
+  }
 }
